ecde07053a7b12d99fbe7d7cb2da334b11f21e2c
Anthony <aengland94@gmail.com>
2019-10-12 14:13:23 -0600

    fixed model bugs

--- models/rental.js
+++ models/rental.js
@@ -2,7 +2,7 @@
 const model = require('./model');
 
 module.exports.getAllRentalsBasic = async () => {
-   const queryString = 'SELECT id, display_name, vacant FROM units';
+   const queryString = 'SELECT id, display_name, vacant FROM rentals';
    const result = await model.query1(queryString);
 
    return (result) ? result.rows : null;
@@ -11,7 +11,7 @@ module.exports.getAllRentalsBasic = async () => {
 module.exports.getAllRentalsSafe = async () => {
    const queryString = 'SELECT id, unit_id, display_name, apt_number, ' + 
       'vacant, lease_expires_on, note, created_on, created_by, updated_on, ' + 
-      'updated_by FROM units';
+      'updated_by FROM rentals';
    const result = await model.query1(queryString);
 
    return (result) ? result.rows : null;
