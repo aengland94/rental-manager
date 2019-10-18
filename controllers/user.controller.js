11d691adc07809609f957a5f90d3603f770c4d66
Anthony <aengland94@gmail.com>
2019-10-17 21:11:55 -0600

    added create user form

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -13,3 +13,32 @@ module.exports.index = async (req, res) => {
 module.exports.show = (req, res) => {
    res.render('admin/index', { title: "User " + req.params.id });
 }
+
+module.exports.create = async (req, res) => {
+   let first_name, last_name, email, username, password;
+
+   if (req.query.first_name) {
+      first_name = req.query.first_name;
+   }
+
+   if (req.query.last_name) {
+      last_name = req.query.last_name;
+   }
+
+   if (req.query.email) {
+      email = req.query.email;
+   }
+
+   if (req.query.username) {
+      username = req.query.username;
+   }
+
+   // TODO: auto-generate temp password for new users
+   password = 'secret';
+
+   if (first_name && last_name && email && username && password) {
+      await User.create(first_name, last_name, email, username, password);
+   }
+
+   res.redirect('/admin/user/');
+}
--- models/user.js
+++ models/user.js
@@ -16,3 +16,12 @@ module.exports.getAllUsersSafe = async () => {
 
    return (result) ? result.rows : null;
 }
+
+module.exports.create = async (first_name, last_name, email, username, password) => {
+   const text = 'INSERT INTO users (first_name, last_name, email, username, ' +
+      'password, created_by) VALUES ($1, $2, $3, $4, $5, $6)';
+   // TODO: have created_by be the user currently signed in
+   const values = [ first_name, last_name, email, username, password, 1 ];
+
+   await model.query2(text, values);
+}
