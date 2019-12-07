ae0915326013b2f7b050de0aa002e3e26e478faa
Anthony <aengland94@gmail.com>
2019-12-07 16:40:43 -0700

    added session usage + started Google SignOn

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 const express = require('express')
 const path = require('path')
 const admin = require('./admin.router')
+const cookieSession = require('cookie-session')
 const PORT = process.env.PORT || 5000
 
 express()
    .use(express.static(path.join(__dirname, 'public')))
+   .use(cookieSession({
+      name: "session",
+      keys: ['fhbh->2|BH10C2|^|fcr/c#'],
+      // Cookie Options
+      maxAge: 1 * 60 * 60 * 1000, // 1 hour
+      httpOnly: true
+   }))
    .set('views', path.join(__dirname, 'views'))
    .set('view engine', 'ejs')
    .use(express.json()) // for parsing application/json
    .use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
-   .get('/', (req, res) => res.render('pages/index'))
+   .get('/', (req, res) => res.render('public/index', { title: "England Rentals" }))
    .use('/admin', admin)
+   .route('/login')
+      .get((req, res) => res.render('public/login', { googleSignInID: process.env.GOOGLE_SIGNIN_CLIENT_ID, title: "Login" }))
+   .route('/oauth2callback')
+      .post((req, res) => res.redirect('/'))
    .listen(PORT, () => console.log(`Listening on ${ PORT }`))
